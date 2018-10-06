db9d99a67c95a0475c98bbb969e384171f9eae93
Sumathi1996 <43901950+Sumathi1996@users.noreply.github.com>
2018-10-06 14:09:49 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        if(n > 0)
+        {
+            System.out.println("The given number "+n+" is Positive");
+        }
+        else if(n < 0)
+        {
+            System.out.println("The given number "+n+" is Negative");
+        }
+        else
+        {
+            System.out.println("The given number "+n+" is neither Positive nor Negative ");
+        }
+    }
+}
